5f43fb3487924b4ec4447ddacef0cfc84f2969de
Sebastian Steins <592313+sebst@users.noreply.github.com>
2023-09-22 17:23:49 +0200

    Change plot.py

--- CLEAN/plot.py
+++ CLEAN/plot.py
@@ -4,6 +4,7 @@ import os
 import json
 import numpy as np
 from matplotlib import pyplot as plt
+import argparse
 
 
 def plotData(filename, barcode=""):
@@ -13,12 +14,15 @@ def plotData(filename, barcode=""):
     y1 = []
     y2 = []
 
+    color = ""
+
     #fig,axes = plt.subplots(3,1)
     fig = plt.figure(figsize=(10,5))
     for line in f:
         #print(line)
         data = line.split(',')
-        col = data[-1]
+        if not color:
+            color = data[-1]
         data[-1] = data[-1][:-1]
         #print(data)
         x.append(float(data[1]))
@@ -50,15 +54,22 @@ def plotData(filename, barcode=""):
     ax.set_ylabel('y-pos [µm]')
     ax.set_ylim(y2.mean()-2.5,y2.mean()+2.5)
 
-    col_str = " – {col}" if col else ""
-    plt.suptitle(f'{filename} - Ser# {barcode}{col_str}')
+    color_string = f' – COL: {color}' if color else ""
+
+    plt.suptitle(f'{filename} - Ser# {barcode}{color_string}')
     plt.tight_layout()
     #plt.ylim((y.mean()-1,y.mean()+1))
     plt.savefig(f'{filename[:-4]}.png')
 
 
-def main():
-    files = os.listdir()
+def main(DIR=""):
+
+    if DIR:
+        files = os.listdir(DIR)
+        DIR = str(DIR)
+    else:    
+        files = os.listdir()
+        DIR = ""
 
     barcodes = {}
     try:
@@ -74,8 +85,11 @@ def main():
             barcode = barcodes.get(ip_sfx, "N/A")
             print(f'opening {f}....')
             print('Barcodes', barcodes)
+            if DIR:
+                f = DIR + "/" + f
             plotData(f, barcode)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="DESC")
     main()
