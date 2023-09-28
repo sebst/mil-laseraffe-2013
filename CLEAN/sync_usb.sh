477a1b3aa69604d29db2fa450724f958ecf8fb9e
Sebastian Steins <592313+sebst@users.noreply.github.com>
2023-09-28 11:23:30 +0200

    sync_usb.sh

--- CLEAN/sync_usb.sh
+++ CLEAN/sync_usb.sh
@@ -1,20 +1,27 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 
-set -o errexit
-set -o nounset
-set -o pipefail
-if [[ "${TRACE-0}" == "1" ]]; then
-    set -o xtrace
-fi
+#set -o errexit
+#set -o nounset
+#set -o pipefail
+#if [[ "${TRACE-0}" == "1" ]]; then
+#    set -o xtrace
+#fi
 
 
 cd "$(dirname "$0")"
 
-
-cp barcodes.json $1/ || echo "" > $1/barcodes.json
-sudo mkdir -p /mnt/usb
+# Mount the PenDrive
+sudo mkdir -p /mnt/usb/
 sudo mount /dev/sda1 /mnt/usb
+# Create the target directory
+sudo mkdir -p /mnt/usb/$1
+# Run the plotter
 python3 plot.py $1
-sudo cp -a $1 /mnt/usb/
+# Copy relevant files
+sudo cp barcodes.json /mnt/usb/$1/ || echo "" > /mnt/usb/$1/barcodes.json
+sudo cp -r $1 /mnt/usb/
+# Unmount the PenDrive
 sudo umount /mnt/usb
+
+# EOF
